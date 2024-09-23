e5b1065fbc860e7348a11b6ec2a583f9f3e6736f
박성현 <qkr970504@gmail.com>
2024-09-23 22:39:12 +0900

    [sec-01] 문장 속 단어

---
+++ src/section01/WordsInSentences.java
@@ -0,0 +1,28 @@
+package section01;
+
+import java.util.Scanner;
+
+/**
+ * 문장 속 단어
+ * 문장 속 가장 긴 단어 출력
+ */
+public class WordsInSentences {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String result = "";
+        for (String s : str.split(" ")) {
+            int length = s.length();
+            if (result.length() < length) {
+                result = s;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
