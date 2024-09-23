db45ed236b02bd5b59bbb34675d52adaadfaff6c
박성현 <qkr970504@gmail.com>
2024-09-23 22:39:00 +0900

    [sec-01] 대소문자 변환

---
+++ src/section01/CaseConversion.java
@@ -0,0 +1,28 @@
+package section01;
+
+import java.util.Scanner;
+
+/**
+ * 대소문자 변환
+ */
+public class CaseConversion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        System.out.println(sb);
+
+        sc.close();
+    }
+}
