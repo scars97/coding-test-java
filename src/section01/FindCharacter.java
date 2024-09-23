0a35db0fa7efab9172b3ea6baa36e3e556d412f7
박성현 <qkr970504@gmail.com>
2024-09-23 22:38:49 +0900

    [sec-01] 문자열 찾기

---
+++ src/section01/FindCharacter.java
@@ -0,0 +1,30 @@
+package section01;
+
+import java.util.Scanner;
+
+/**
+ * 문자열 찾기
+ */
+public class FindCharacter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+        char keyword = sc.next().charAt(0);
+
+        String lowerCaseOfWord = word.toLowerCase();
+        char c = Character.toLowerCase(keyword);
+
+        int count = 0;
+        for (char t : lowerCaseOfWord.toCharArray()) {
+            if (t == c) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
