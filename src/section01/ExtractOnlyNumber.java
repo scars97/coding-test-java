8300325a8bb2ad9bfb5e9322c72b3cf3e1778909
박성현 <qkr970504@gmail.com>
2024-09-26 23:05:10 +0900

    [sec-01] 숫자만 추출

---
+++ src/section01/ExtractOnlyNumber.java
@@ -0,0 +1,28 @@
+package section01;
+
+import java.util.Scanner;
+
+/**
+ * 숫자만 추출
+ *
+ * 문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+ * 만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
+ * 추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+ *
+ * in : g0en2T0s8eSoft
+ * out : 208
+ */
+public class ExtractOnlyNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        String tmp = str.replaceAll("[^0-9]", "");
+        Integer num = Integer.parseInt(tmp);
+
+        System.out.println(num);
+        sc.close();
+    }
+}
