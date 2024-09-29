83d547ed3bf7899be20bfe0caf93d9b44dbe4e07
박성현 <qkr970504@gmail.com>
2024-09-29 21:02:33 +0900

    [sec-01] 암호 - 리팩토링
    - 메서드 추출

--- src/section01/Password.java
+++ src/section01/Password.java
@@ -18,21 +18,42 @@ import java.util.Scanner;
  */
 public class Password {
 
+    public static final String BINARY_ONE_CHAR = "#";
+    public static final String BINARY_ZERO_CHAR = "*";
+    public static final int CHARACTER_BINARY_LENGTH = 7;
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int pwdLength = sc.nextInt();
         String pwdCodes = sc.next();
 
-        String[] specialCodes = new String[pwdLength];
-        for (int i = 0; i < specialCodes.length; i++) {
-            int startIndex = i * 7;
-            int endIndex = startIndex + 7;
-            specialCodes[i] = pwdCodes.substring(startIndex, endIndex);
-        }
+        String[] specialCodes = splitStringToPwdCodes(pwdLength, pwdCodes);
+        convertSpecialCodesToBinaryString(specialCodes);
+        int[] binaryArray = convertBinaryStringToDecimal(specialCodes);
 
+        String result = convertDecimalToString(binaryArray);
+        System.out.println(result);
+        sc.close();
+    }
+
+    private static String convertDecimalToString(int[] binaryArray) {
+        StringBuilder sb = new StringBuilder();
+        for (int binary : binaryArray) {
+            sb.append((char) binary);
+        }
+        return sb.toString();
+    }
+
+    private static int[] convertBinaryStringToDecimal(String[] specialCodes) {
+        int[] binaryArray = new int[specialCodes.length];
+        for (int i = 0; i < specialCodes.length; i++) {
+            binaryArray[i] = Integer.parseInt(specialCodes[i], 2);
+        }
+        return binaryArray;
+    }
+
+    private static void convertSpecialCodesToBinaryString(String[] specialCodes) {
         for (int i = 0; i < specialCodes.length; i++) {
             String specialCode = specialCodes[i];
 
@@ -40,23 +61,23 @@ public class Password {
             for (int j = 0; j < specialCode.length(); j++) {
                 String code = String.valueOf(specialCode.charAt(j));
 
-                if ("#".equals(code)) {
+                if (BINARY_ONE_CHAR.equals(code)) {
                     sb.append("1");
-                } else if ("*".equals(code)) {
+                } else if (BINARY_ZERO_CHAR.equals(code)) {
                     sb.append("0");
                 }
             }
             specialCodes[i] = sb.toString();
         }
+    }
 
-
-        StringBuilder sb = new StringBuilder();
-        for (String specialCode : specialCodes) {
-            int binary = Integer.parseInt(specialCode, 2);
-            char c = (char) binary;
-
-            sb.append(c);
+    private static String[] splitStringToPwdCodes(int pwdLength, String pwdCodes) {
+        String[] specialCodes = new String[pwdLength];
+        for (int i = 0; i < specialCodes.length; i++) {
+            int startIndex = i * CHARACTER_BINARY_LENGTH;
+            int endIndex = startIndex + CHARACTER_BINARY_LENGTH;
+            specialCodes[i] = pwdCodes.substring(startIndex, endIndex);
         }
-        System.out.println(sb);
+        return specialCodes;
     }
 }
