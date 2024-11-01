9b55d9a74ea315d771dd09306fdaa2fed1fa2882
박성현 <qkr970504@gmail.com>
2024-11-01 14:05:45 +0900

    [99클럽] 백준 24444번 너비 우선 탐색1

--- src/spartacodingclub/mission/week1/BFS1.java
+++ src/spartacodingclub/mission/week1/BFS1.java
@@ -5,9 +5,8 @@ import java.util.*;
 public class BFS1 {
 
     static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+    static int[] visited;
     static int visitedIdx = 1;
-    static int[] result;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,8 +16,7 @@ public class BFS1 {
         int r = sc.nextInt();
 
         graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        result = new int[n + 1];
+        visited = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
@@ -38,7 +36,7 @@ public class BFS1 {
         bfs(r);
 
         for (int i = 1; i <= n; i++) {
-            System.out.println(result[i]);
+            System.out.println(visited[i]);
         }
         sc.close();
     }
@@ -46,18 +44,15 @@ public class BFS1 {
     static void bfs(int r) {
         Queue<Integer> queue = new LinkedList<>();
 
-        result[visitedIdx++] = r;
-        visited[r] = true;
+        visited[r] = visitedIdx++;
         queue.add(r);
 
         while (!queue.isEmpty()) {
             int x = queue.remove();
 
             for (int node : graph[x]) {
-                if (!visited[node]) {
-                    visited[node] = true;
-                    // 방문 순서 저장이 아닌 방문한 노드 번호를 저장하고 있다.
-                    result[visitedIdx++] = node;
+                if (visited[node] == 0) {
+                    visited[node] = visitedIdx++;
                     queue.add(node);
                 }
             }
