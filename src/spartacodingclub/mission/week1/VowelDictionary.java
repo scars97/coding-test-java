af3c1b5cd8e385da115843695f08e9ff96715f78
박성현 <qkr970504@gmail.com>
2024-11-03 22:26:53 +0900

    [99클럽] 프로그래머스 모음 사전

---
+++ src/spartacodingclub/mission/week1/VowelDictionary.java
@@ -0,0 +1,29 @@
+package spartacodingclub.mission.week1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VowelDictionary {
+
+    private static final String[] ALPHABETS = {"A", "E", "I", "O", "U"};
+    private static final List<String> DICTIONARY = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String word = "EIO";
+        System.out.println(solution(word));
+    }
+
+    static int solution(String word) {
+        generateWords("");
+        return DICTIONARY.indexOf(word);
+    }
+
+    static void generateWords(String word) {
+        if (word.length() > 5) return;
+        DICTIONARY.add(word);
+
+        for (int i = 0; i < ALPHABETS.length; i++) {
+            generateWords(word + ALPHABETS[i]);
+        }
+    }
+}
