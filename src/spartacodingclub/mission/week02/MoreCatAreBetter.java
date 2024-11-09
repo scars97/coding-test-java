5e11cd0e1f542bc1f67eca85092b8ec788f1ee1d
박성현 <qkr970504@gmail.com>
2024-11-09 15:57:40 +0900

    [99클럽] 백준 27961번 고양이
    - 반복문 제거

--- src/spartacodingclub/mission/week02/MoreCatAreBetter.java
+++ src/spartacodingclub/mission/week02/MoreCatAreBetter.java
@@ -9,7 +9,6 @@ public class MoreCatAreBetter {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         long n = Long.parseLong(br.readLine());
         int action = 0;
@@ -17,9 +16,9 @@ public class MoreCatAreBetter {
         while (catCount < n) {
             long remainingCount = n - catCount;
 
-            long generate = catCount + GENERATE_COUNT;
-            long copyCount = copy(remainingCount);
+            long copyCount = Math.min(catCount, remainingCount);
             long copy = catCount + copyCount;
+            long generate = catCount + GENERATE_COUNT;
 
             long max = generate >= copy ? GENERATE_COUNT : copyCount;
 
@@ -27,19 +26,7 @@ public class MoreCatAreBetter {
             action++;
         }
 
-        bw.write(String.valueOf(action));
-        bw.flush();
+        System.out.println(action);
         br.close();
-        bw.close();
-    }
-
-    static long copy(long remainingCount) {
-        long result = 0;
-        for (long i = 0; i <= catCount; i++) {
-            if (i <= remainingCount) {
-                result = Math.max(result, i);
-            }
-        }
-        return result;
     }
 }
