35320590497bd03c5eb5dde555c158762dc28574
박성현 <qkr970504@gmail.com>
2024-11-17 15:45:50 +0900

    [99클럽] 프로그래머스 카펫 - 리팩토링

--- src/spartacodingclub/mission/week03/Carpet.java
+++ src/spartacodingclub/mission/week03/Carpet.java
@@ -14,18 +14,12 @@ public class Carpet {
             int[] answer = new int[2];
 
             int totalSize = brown + yellow;
-            int col = 3;
-            while (totalSize / col >= col) {
-                if (totalSize % col == 0) {
-                    // 갈색 테두리는 1줄.
-                    // (가로 - 2) * (세로 - 2) = yellow 개수
-                    if ((totalSize / col - 2) * (col - 2) == yellow) {
-                        answer[0] = totalSize / col;
-                        answer[1] = col;
-                        break;
-                    }
+            for (int col = 3; col <= totalSize; col++) {
+                if ((totalSize / col - 2) * (col - 2) == yellow) {
+                    answer[0] = totalSize / col;
+                    answer[1] = col;
+                    break;
                 }
-                col++;
             }
 
             return answer;
