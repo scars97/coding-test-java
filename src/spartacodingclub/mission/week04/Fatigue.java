e1a1e44e058e80bf84919ce14f9a87adc35f258d
박성현 <qkr970504@gmail.com>
2024-11-18 21:46:15 +0900

    [99클럽] 프로그래머스 피로도

--- src/spartacodingclub/mission/week04/Fatigue.java
+++ src/spartacodingclub/mission/week04/Fatigue.java
@@ -1,63 +1,37 @@
 package spartacodingclub.mission.week04;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Fatigue {
 
     public static void main(String[] args) {
-
+        int k = 80;
         int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
-        int solution = new Solution().solution(80, dungeons);
+        int solution = new Solution().solution(k, dungeons);
         System.out.println(solution);
     }
 
     static class Solution {
+
+        public boolean[] visited;
+        public int result = 0;
         public int solution(int k, int[][] dungeons) {
+            visited = new boolean[dungeons.length];
 
-            List<DungeonsFatigue> dungeonList = new ArrayList<>();
-            for (int i = 0; i < dungeons.length; i++) {
-                dungeonList.add(new DungeonsFatigue(dungeons[i][0], dungeons[i][1]));
-            }
-            Collections.sort(dungeonList);
+            dfs(0, k, dungeons);
 
-            int count = 0;
-            for (DungeonsFatigue dungeon : dungeonList) {
-                if (k < dungeon.required) {
-                    continue;
-                }
+            return result;
+        }
 
-                if (k >= dungeon.consumption) {
-                    k -= dungeon.consumption;
-                    count++;
+        void dfs(int depth, int fatigue, int[][] dg) {
+            for (int i = 0; i < dg.length; i++) {
+                if (!visited[i] && dg[i][0] <= fatigue) {
+                    visited[i] = true;
+                    dfs(depth + 1, fatigue - dg[i][1], dg);
+                    visited[i] = false; // 다른 던전부터 탐험하는 경우도 있기 때문에 방문 초기화
                 }
             }
 
-            return count;
+            result = Math.max(result, depth);
         }
 
-    }
-
-    static class DungeonsFatigue implements Comparable<DungeonsFatigue> {
-        public int required;
-        public int consumption;
-
-        public DungeonsFatigue(int required, int consumption) {
-            this.required = required;
-            this.consumption = consumption;
-        }
-
-        @Override
-        public int compareTo(DungeonsFatigue o) {
-
-            int other = o.required - o.consumption;
-            int current = this.required - this.consumption;
-
-            if (other == current) {
-                return Integer.compare(o.required, this.required);
-            }
-            return Integer.compare(other, current);
-        }
     }
 }
