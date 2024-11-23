5939db5ae4732cd8952e953171f7a5481b21849c
박성현 <qkr970504@gmail.com>
2024-11-23 15:09:55 +0900

    [99클럽] 백준 11722번 가장 긴 감소하는 부분 수열
    - LDS 적용

--- src/spartacodingclub/mission/week04/DecreasePartialSequences.java
+++ src/spartacodingclub/mission/week04/DecreasePartialSequences.java
@@ -3,6 +3,7 @@ package spartacodingclub.mission.week04;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*
@@ -14,26 +15,49 @@ public class DecreasePartialSequences {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        int[] sequences = new int[N];
-        int[] dp = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            sequences[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (sequences[j] > sequences[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            result = Math.max(result, dp[i]);
-        }
-
+        int result = getLdsLength(arr);
         System.out.println(result);
         br.close();
+    }
+
+    private static int getLdsLength(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            int el = arr[i];
+            if (list.get(list.size() - 1) > el) {
+                list.add(el);
+            } else {
+                int idx = binarySearch(list, el);
+                list.set(idx, el);
+            }
+        }
+
+        return list.size();
+    }
+
+    private static int binarySearch(ArrayList<Integer> list, int el) {
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) == el) {
+                return mid;
+            } else if (list.get(mid) > el) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
     }
 }
