dc8e85c3c49825c4ebc1bbde162ba40cdbd625b3
박성현 <qkr970504@gmail.com>
2024-11-26 14:48:50 +0900

    [99클럽] 백준 1965번 상자 넣기
    - 이분탐색 적용

--- src/spartacodingclub/mission/week05/InputBox.java
+++ src/spartacodingclub/mission/week05/InputBox.java
@@ -3,6 +3,7 @@ package spartacodingclub.mission.week05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*
@@ -14,25 +15,50 @@ public class InputBox {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] boxes = new int[n];
-        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             boxes[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
         }
         br.close();
 
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (boxes[j] < boxes[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            result = Math.max(result, dp[i]);
-        }
-
+        int result = lis(boxes, n);
         System.out.println(result);
     }
+
+    private static int lis(int[] boxes, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(boxes[0]);
+
+        for (int i = 1; i < n; i++) {
+            int tmp = boxes[i];
+            if (list.get(list.size() - 1) < tmp) {
+                list.add(tmp);
+            } else {
+                int index = binarySearch(list, tmp);
+                list.set(index, tmp);
+            }
+        }
+
+        return list.size();
+    }
+
+    private static int binarySearch(ArrayList<Integer> list, int tmp) {
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) == tmp) {
+                return mid;
+            } else if (list.get(mid) < tmp) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
 }
